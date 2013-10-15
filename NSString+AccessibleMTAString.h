4b027dcad4924a16433bda6ae1aa0d171f8664a6
Michael Sanderson <mikexcite@gmail.com>
2013-10-14 23:12:47 -0400

    Added iOS NSString Categories
    NSString+AccessibleMTAString.h
    NSString+AccessibleMTAString.m

---
+++ NSString+AccessibleMTAString.h
@@ -0,0 +1,16 @@
+//
+//  NSString+AccessibleMTAString.h
+//  Subway AR
+//
+//  Created by Michael Sanderson 2 on 10/14/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (AccessibleMTAString)
+
+-(NSString*)convertToVoiceOverReadyString;
+
+
+@end
---
+++ NSString+AccessibleMTAString.m
@@ -0,0 +1,26 @@
+//
+//  NSString+AccessibleMTAString.m
+//  Subway AR
+//
+//  Created by Michael Sanderson 2 on 10/14/13.
+//
+//
+
+#import "NSString+AccessibleMTAString.h"
+
+@implementation NSString (AccessibleMTAString)
+
+
+-(NSString*)convertToVoiceOverReadyString
+{
+
+
+
+	
+
+
+
+}
+
+
+@end
